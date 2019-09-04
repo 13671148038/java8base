0134a10c062718a6a014d61255b0c6cdf0ef7396
zhushaopeng <zhushaopeng@ybm100.com>
2019-09-04 10:04:13 +0800

    想是

--- src/main/java/com/balimiao/App.java
+++ src/main/java/com/balimiao/App.java
@@ -1,10 +1,29 @@
 package com.balimiao;
 
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(LocalDate.now().toString());
+        List<String> list1 = new ArrayList<>();
+        list1.add("2");
+        list1.add("5");
+        List<String> list2 = new ArrayList<>();
+        list2.add("567");
+        list2.add("5tty");
+        List<String> list3 = new ArrayList<>();
+        list3.add("fvdsf");
+        list3.add("fvdsf");
+        list3.add("fvdsf");
+        List<String> collect = Stream.of(list1, list2, list3).flatMap(c -> c.stream()).collect(Collectors.toList());
+        System.out.println(collect.size());
     }
+
+
 }
--- src/main/java/com/balimiao/lambdademo/MyLambda.java
+++ src/main/java/com/balimiao/lambdademo/MyLambda.java
@@ -1,11 +1,8 @@
 package com.balimiao.lambdademo;
 
-import com.balimiao.lambdademo.mm.Adc;
-import jdk.nashorn.internal.parser.JSONParser;
-
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collector;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,13 +16,12 @@ public class MyLambda {
         list.add("f");
 
         List<List<Integer>> list2 = new ArrayList<>();
-
         List<Integer> integers = Arrays.asList(3, 4, 5, 6, 7);
         List<Integer> numbers = Arrays.asList(9, 9, 8);
         list2.add(integers);
         list2.add(numbers);
-        Integer reduce  = integers.stream().reduce((integer, integer2) -> integer + integer2).get();
-        System.out.println("reduce"+reduce);
+        Integer reduce = integers.stream().reduce((integer, integer2) -> integer + integer2).get();
+        System.out.println("reduce" + reduce);
 
 
 //        List<Integer> collect = list2.stream().flatMap(c ->
@@ -34,11 +30,12 @@ public class MyLambda {
 //        collect.forEach(c ->
 //                System.out.println(c)
 //        );
-
-        List<?> collect = Stream.of(list, list2.stream().flatMap(d->
-                d.stream()).collect(Collectors.toList())).flatMap(c ->
-                c.stream()).collect(Collectors.toList());
-        collect.forEach(c->
+        List<?> collect = Stream.of(list, list2.stream()
+                .flatMap(d -> d.stream())
+                .collect(Collectors.toList()))
+                .flatMap(c -> c.stream())
+                .collect(Collectors.toList());
+        collect.forEach(c ->
                 System.out.println(c));
     }
 
