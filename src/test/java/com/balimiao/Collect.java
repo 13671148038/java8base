06690ea779abcfb6142855ebc43fc36b3a32abc9
zhushaopeng <zhushaopeng@ybm100.com>
2019-10-23 15:28:02 +0800

    修改注释信息sc四渡赤水

---
+++ src/test/java/com/balimiao/Collect.java
@@ -0,0 +1,45 @@
+package com.balimiao;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @Author zhushaopeng
+ * @Date 2019/9/30
+ **/
+public class Collect {
+
+    @Test
+    public void a() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(2);
+        list.add(2);
+        list.sort((a, b) -> a.compareTo(b));
+        list.forEach(c -> System.out.println(c));
+    }
+
+    @Test
+    public void aa() {
+        List<Map<String, String>> list = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+        map.put("aaaaa", "aaaaaa");
+        list.add(map);
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("bbbbb", "bbbbb");
+        list.add(map2);
+        Map<String, String> map3 = new HashMap<>();
+        map3.put("aaaaa", "aaaaaa");
+        list.add(map3);
+        Set<Map<String, String>> collect = list.stream().collect(Collectors.toSet());
+        System.out.println(collect.size());
+    }
+
+    @Test
+    public  void aaa(){
+
+    }
+
+}
