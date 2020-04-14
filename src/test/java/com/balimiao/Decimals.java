962447ff71b830ebb853eff00cc8005687ad9d72
zhushaopeng <zhushaopeng@ybm100.com>
2020-04-14 16:44:51 +0800

    '添加BigDecimal'

--- src/test/java/com/balimiao/Decimals.java
+++ src/test/java/com/balimiao/Decimals.java
@@ -2,6 +2,9 @@ package com.balimiao;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * @Description: TODO
  * @Author zhushaopeng
@@ -18,5 +21,16 @@ public class Decimals {
         int c = 3, d = 2;
         System.out.println(c / d);
         System.out.println(c % d);
+    }
+
+    @Test
+    public void b() {
+        BigDecimal decimal = new BigDecimal("2020.3625236").
+                multiply(new BigDecimal("2020.3625236")). //乘以
+                divide(new BigDecimal("2")).  //除以
+                add(new BigDecimal("3")).  //加
+                subtract(new BigDecimal("62")). //减法
+                setScale(2, RoundingMode.HALF_UP); //保留两位小数,四舍五入
+        System.out.println(decimal);
     }
 }
