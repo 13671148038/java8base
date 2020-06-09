5741a4b1138e6d7517ea568f19622902b26e0a05
zhushaopeng <zhushaopeng@ybm100.com>
2020-06-09 15:26:13 +0800

    ''

--- src/main/java/com/balimiao/App.java
+++ src/main/java/com/balimiao/App.java
@@ -5,7 +5,12 @@ package com.balimiao;
  */
 public class App {
     public static void main(String[] args) {
-
+        Integer i = 128;
+        Integer j = 128;
+        System.out.println(i == j);
+        int a = 444;
+        int b = 444;
+        System.out.println(a == b);
     }
 
 }
