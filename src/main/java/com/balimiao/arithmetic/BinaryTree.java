1c5d17fd034b6cd7de54abb36f5b5f412f381051
zhushaopeng <13671148038@163.com>
2021-11-30 11:30:25 +0800

    xx

--- src/main/java/com/balimiao/arithmetic/BinaryTree.java
+++ src/main/java/com/balimiao/arithmetic/BinaryTree.java
@@ -1,9 +1,6 @@
 package com.balimiao.arithmetic;
 
-import com.alibaba.fastjson.JSON;
 import com.balimiao.arithmetic.data.BinaryTreeNode;
-
-import java.util.Stack;
 
 /**
  * @Description: 二叉树的遍历
@@ -15,42 +12,71 @@ public class BinaryTree {
 
     public static void main(String[] args) {
         BinaryTreeNode binaryTreeNode = generateTree();
-        //遍历二叉树,前序
-        Stack<BinaryTreeNode> stack = new Stack<>();
-        stack.push(binaryTreeNode);
-        while (!stack.empty()) {
-            BinaryTreeNode pop = stack.pop();
-            System.out.println(pop.getValue());
-            if (pop.getRightNode() != null) {
-                stack.push(pop.getRightNode());
-            }
-            if (pop.getLeftNode() != null) {
-                stack.push(pop.getLeftNode());
-            }
+//        //遍历二叉树,前序
+//        Stack<BinaryTreeNode> stack = new Stack<>();
+//        stack.push(binaryTreeNode);
+//        while (!stack.empty()) {
+//            BinaryTreeNode pop = stack.pop();
+//            System.out.println(pop.getValue());
+//            if (pop.getRightNode() != null) {
+//                stack.push(pop.getRightNode());
+//            }
+//            if (pop.getLeftNode() != null) {
+//                stack.push(pop.getLeftNode());
+//            }
+//        }
+//        pre(binaryTreeNode);
+
+        in(binaryTreeNode);
+    }
+
+    /**
+     * 前序遍历
+     *
+     * @param node
+     */
+    public static void pre(BinaryTreeNode node) {
+        if (node == null) {
+            return;
         }
+        System.out.println(node.getValue());
+        pre(node.getLeftNode());
+        pre(node.getRightNode());
+    }
 
-        //遍历二叉树,中
-        Stack<BinaryTreeNode> stack2 = new Stack<>();
-        stack.push(binaryTreeNode);
-        while (!stack.empty()) {
-            BinaryTreeNode pop = stack.pop();
-            if (pop.getRightNode() != null) {
-                stack.push(pop.getRightNode());
-            }
-            if (pop.getLeftNode() != null) {
-                stack.push(pop.getLeftNode());
-            }
+    /**
+     * 序中遍历
+     *
+     * @param node
+     */
+    public static void in(BinaryTreeNode node) {
+        if (node == null) {
+            return;
         }
+        pre(node.getLeftNode());
+        System.out.println(node.getValue());
+        pre(node.getRightNode());
+    }
 
-
-
+    /**
+     * 后中遍历
+     *
+     * @param node
+     */
+    public static void pos(BinaryTreeNode node) {
+        if (node == null) {
+            return;
+        }
+        pre(node.getLeftNode());
+        pre(node.getRightNode());
+        System.out.println(node.getValue());
     }
 
 
     public static BinaryTreeNode generateTree() {
 
         BinaryTreeNode root = new BinaryTreeNode();
-        Integer[] nodeValue = new Integer[]{7, 3, 89, 65, 3, 87, 5, 3, 7, 9, 0, 3};
+        Integer[] nodeValue = new Integer[]{7, 3, 89, 65, 8, 87, 5, 2, 7, 9, 0, 1};
         for (Integer value : nodeValue) {
 
 
--- src/test/java/com/balimiao/Atest.java
+++ src/test/java/com/balimiao/Atest.java
@@ -1,28 +1,11 @@
 package com.balimiao;
 
-import com.alibaba.fastjson.JSON;
-import com.balimiao.pojo.UserVisitMsg;
-import javazoom.jl.decoder.Bitstream;
-import javazoom.jl.decoder.Header;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 
 @Slf4j
 public class Atest {
-    private static final ThreadPoolExecutor pool = new ThreadPoolExecutor(8, 8, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue());
-
     @Test
     public void bbbbccc() {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -51,88 +34,13 @@ public class Atest {
     }
 
     @Test
-    public void bbbb() {
-//        System.out.println(177534474465902592L / 10000 % 256);
-//        System.out.println(360207024878780416L % 512);
+    public void aa() {
+        String aa = "sdc/sdcs";
+        int i = aa.lastIndexOf("/");
+        String substring = aa.substring(i + 1);
+        System.out.println(substring);
 
-        List<List<UserVisitMsg>> lists = new ArrayList<>();
-        List<UserVisitMsg> list = new ArrayList<>();
-        lists.add(list);
-        UserVisitMsg userVisitMsg = new UserVisitMsg();
-        userVisitMsg.setUserId(33L);
-        list.add(userVisitMsg);
-        lists.stream().flatMap(Collection::stream).forEach(c -> {
-            c.setChannel("343434");
-            c.setUserId(5555l);
-        });
-        System.out.println(JSON.toJSONString(lists));
 
     }
-
-
-    @Test
-    public void aas() throws Exception {
-
-        StringBuffer stringBuffer = new StringBuffer("https://sf-book-audio.oss-cn-beijing.aliyuncs.com/1632908405691_E1027.mp3");
-        stringBuffer.delete(0, stringBuffer.indexOf("//") + 2);
-        String bucketName = stringBuffer.substring(0, stringBuffer.indexOf("."));
-        String key = stringBuffer.substring(stringBuffer.indexOf("/") + 1);
-
-        System.out.println(bucketName);
-        System.out.println(key);
-//        URI uri = new URI("https://sf-book-audio.oss-cn-beijing.aliyuncs.com/1632908405691_E1027.mp3");
-//        URI uri1 = URI.create("https://sf-book-audio.oss-cn-beijing.aliyuncs.com/1632908405691_E1027.mp3");
-//        InputStream inputStream = new URL("https://sf-book-audio.oss-cn-beijing.aliyuncs.com/1632908405691_E1027.mp3").openStream();
-//        URL urlfile = new URL("https://sf-book-audio.oss-cn-beijing.aliyuncs.com/1632908405691_E1027.mp3");
-//        URI uri = urlfile.toURI();
-//        File file = new File("C:/Users/admin/Desktop/数据/歌曲动画/seehear/079/15569/0.mp3");
-//        MP3File mp3File = new MP3File("C:/Users/admin/Desktop/数据/歌曲动画/seehear/079/15569/0.mp3");
-//        MP3AudioHeader audioHeader = (MP3AudioHeader) mp3File.getAudioHeader();
-//
-//        String strLen = audioHeader.getTrackLengthAsString();
-//        System.out.println(strLen);
-//
-//        System.out.println(JSON.toJSONString(audioHeader));
-//
-//        int intLen = audioHeader.getTrackLength();
-//        System.out.println(intLen);
-
-    }
-
-    @Test
-    public void aa() throws Exception {
-        URL urlfile = new URL("https://sf-book-audio.oss-cn-beijing.aliyuncs.com/1632908405691_E1027.mp3");
-        URLConnection con = null;
-        try {
-            con = urlfile.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        int b = con.getContentLength();
-        BufferedInputStream bis = new BufferedInputStream(con.getInputStream());
-        Bitstream bt = new Bitstream(bis);
-        Header h = bt.readFrame();
-        int time = (int) h.total_ms(b);
-        System.out.println(time);
-
-    }
-
-    @Test
-    public void aa3() {
-        int a = 8;
-        switch (a) {
-            case 8: {
-                return;
-            }
-            case 1: {
-            }
-            case 6: {
-            }
-            default:
-                break;
-        }
-        System.out.println(222);
-    }
-
 
 }
